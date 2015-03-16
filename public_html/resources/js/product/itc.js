0611bdab059776fe29bb9077be8a5b583dcc9f03
app2641 <app2641@gmail.com>
2015-03-16 16:47:27 +0900

    ITCページにアナリティクスを設定

--- public_html/resources/js/product/itc.js
+++ public_html/resources/js/product/itc.js
@@ -1,6 +1,11 @@
 
-$(function () {
-    $('button.itc-entry').click(function () {
-        console.log(this);
-    });
-});
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-39037198-7']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
